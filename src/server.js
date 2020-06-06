fe2d0e0f5c4a4e9d10d365290974a385d82c9d13
Andre Ricardo <andrericardoweb@gmail.com>
2020-06-06 16:53:25 -0300

    configure application paths

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 const express = require("express")
 const server = express()
 
+//configure application paths
+//home page
+server.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html")
+})
+
 //start server
 server.listen(3000)
